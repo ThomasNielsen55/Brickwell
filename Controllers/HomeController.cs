027374d5e38ed6d6d5de7e128a908afcd468d773
nicholas18thomas <nicholas18thomas@gmail.com>
2024-04-11 14:04:47 -0600

    onnnx final

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -116,19 +116,28 @@ namespace BrickedUpBrickBuyer.Controllers
             return View(Productguy);
         }
 
-        public IActionResult Orders() 
+        public IActionResult Orders(Order order) 
         {
-            var records = (from order in _brickRepository.Orders
-                           join customer in _brickRepository.Customers
-                           on order.CustomerId equals customer.customer_ID
-                           orderby order.Date descending
-                           select new
-                           {
-                               Customer = customer,
-                               Order = order
-                           }).Take(20);
+            //var records = (from order in _brickRepository.Orders
+            //               join customer in _brickRepository.Customers
+            //               on order.CustomerId equals customer.customer_ID
+            //               orderby order.Date descending
+            //               select new
+            //               {
+            //                   Customer = customer,
+            //                   Order = order
+            //               }).Take(20);
 
-            var predictions = new List<OrderPrediction>(); // Need to figure out what this will be...
+            //   var predictions = new List<OrderPrediction>(); // Need to figure out what this will be...
+
+            var record = new RecordViewModel()
+            {
+                Order = order,
+                Customer = _brickRepository.Customers
+                .Where(x => x.customer_ID == order.CustomerId)
+                .FirstOrDefault()
+            };
+
 
             var class_type_dict = new Dictionary<int, string>
                 {
@@ -136,74 +145,93 @@ namespace BrickedUpBrickBuyer.Controllers
                     {1, "Fraud" }
                 };
 
-            foreach (var record in records)
-            {
-                // Make the data compatible with the models
-                var input = new List<float>
-                    {
-                        (float)record.Customer.age,
-                        (float)record.Order.Time,
-                        (float)(record.Order.Amount ?? 0),
+            //foreach (var record in records)
+            //{
+            // Make the data compatible with the models
+            var input = new List<float>
+                {
+                    (float)record.Customer.age,
+                    (float)record.Order.Time,
+                    (float)(record.Order.Amount ?? 0),
 
-                        // Check the dummy data
-                        record.Customer.country_of_residence == "India" ? 1 : 0,
-                        record.Customer.country_of_residence == "Russia" ? 1 : 0,
-                        record.Customer.country_of_residence == "USA" ? 1 : 0,
-                        record.Customer.country_of_residence == "UnitedKingdom" ? 1 : 0,
+                    // Check the dummy data
+                    record.Customer.country_of_residence == "India" ? 1 : 0,
+                    record.Customer.country_of_residence == "Russia" ? 1 : 0,
+                    record.Customer.country_of_residence == "USA" ? 1 : 0,
+                    record.Customer.country_of_residence == "UnitedKingdom" ? 1 : 0,
 
-                        record.Customer.gender == "M" ? 1 : 0,
+                    record.Customer.gender == "M" ? 1 : 0,
 
-                        record.Order.DayOfWeek == "Mon" ? 1 : 0,
-                        record.Order.DayOfWeek == "Sat" ? 1 : 0,
-                        record.Order.DayOfWeek == "Sun" ? 1 : 0,
-                        record.Order.DayOfWeek == "Thu" ? 1 : 0,
-                        record.Order.DayOfWeek == "Tue" ? 1 : 0,
-                        record.Order.DayOfWeek == "Wed" ? 1 : 0,
+                    record.Order.DayOfWeek == "Mon" ? 1 : 0,
+                    record.Order.DayOfWeek == "Sat" ? 1 : 0,
+                    record.Order.DayOfWeek == "Sun" ? 1 : 0,
+                    record.Order.DayOfWeek == "Thu" ? 1 : 0,
+                    record.Order.DayOfWeek == "Tue" ? 1 : 0,
+                    record.Order.DayOfWeek == "Wed" ? 1 : 0,
 
-                        record.Order.EntryMode == "PIN" ? 1 : 0,
-                        record.Order.EntryMode == "Tap" ? 1 : 0,
+                    record.Order.EntryMode == "PIN" ? 1 : 0,
+                    record.Order.EntryMode == "Tap" ? 1 : 0,
 
-                        record.Order.TypeOfTransaction == "Online" ? 1 : 0,
-                        record.Order.TypeOfTransaction == "POS" ? 1 : 0,
-                        record.Order.TypeOfTransaction == "Online" ? 1 : 0,
+                    record.Order.TypeOfTransaction == "Online" ? 1 : 0,
+                    record.Order.TypeOfTransaction == "POS" ? 1 : 0,
+                    record.Order.TypeOfTransaction == "Online" ? 1 : 0,
 
-                        record.Order.CountryOfTransaction == "India" ? 1 : 0,
-                        record.Order.CountryOfTransaction == "Russia" ? 1 : 0,
-                        record.Order.CountryOfTransaction == "USA" ? 1 : 0,
-                        record.Order.CountryOfTransaction == "UnitedKingdom" ? 1 : 0,
+                    record.Order.CountryOfTransaction == "India" ? 1 : 0,
+                    record.Order.CountryOfTransaction == "Russia" ? 1 : 0,
+                    record.Order.CountryOfTransaction == "USA" ? 1 : 0,
+                    record.Order.CountryOfTransaction == "UnitedKingdom" ? 1 : 0,
 
-                        record.Order.ShippingAddress == "India" ? 1 : 0,
-                        record.Order.ShippingAddress == "Russia" ? 1 : 0,
-                        record.Order.ShippingAddress == "USA" ? 1 : 0,
-                        record.Order.ShippingAddress == "UnitedKingdom" ? 1 : 0,
+                    record.Order.ShippingAddress == "India" ? 1 : 0,
+                    record.Order.ShippingAddress == "Russia" ? 1 : 0,
+                    record.Order.ShippingAddress == "USA" ? 1 : 0,
+                    record.Order.ShippingAddress == "UnitedKingdom" ? 1 : 0,
                     
-                        record.Order.Bank == "HSBC" ? 1 : 0,
-                        record.Order.Bank == "Halifax" ? 1 : 0,
-                        record.Order.Bank == "Lloyds" ? 1 : 0,
-                        record.Order.Bank == "Metro" ? 1 : 0,
-                        record.Order.Bank == "Monzo" ? 1 : 0,
-                        record.Order.Bank == "RBS" ? 1 : 0,
+                    record.Order.Bank == "HSBC" ? 1 : 0,
+                    record.Order.Bank == "Halifax" ? 1 : 0,
+                    record.Order.Bank == "Lloyds" ? 1 : 0,
+                    record.Order.Bank == "Metro" ? 1 : 0,
+                    record.Order.Bank == "Monzo" ? 1 : 0,
+                    record.Order.Bank == "RBS" ? 1 : 0,
 
-                        record.Order.TypeOfCard == "Visa" ? 1 : 0
-                    };
-
-                var inputTensor = new DenseTensor<float>(input.ToArray(), new[] { 1, input.Count });
-
-                var inputs = new List<NamedOnnxValue>
-                {
-                    NamedOnnxValue.CreateFromTensor("float_input", inputTensor)
+                    record.Order.TypeOfCard == "Visa" ? 1 : 0
                 };
-                string predictionResult;
-                using (var results = _session.Run(inputs))
+
+            var inputTensor = new DenseTensor<float>(input.ToArray(), new[] { 1, input.Count });
+
+            var inputs = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", inputTensor)
+            };
+
+            var Fraud = new int();
+
+            using (var results = _session.Run(inputs))
+            {
+                var prediction = results.FirstOrDefault(item => item.Name == "output_label")?.AsTensor<long>().ToArray();
+                if (prediction != null && prediction.Length > 0)
                 {
-                    var prediction = results.FirstOrDefault(item => item.Name == "output_label")?.AsTensor<long>().ToArray();
-                    predictionResult = prediction != null && prediction.length > 0 ? class_type_dict.GetValueOrDefault((int)prediction[0], "Unknown") : "Error in Prediction";
+                    // Use the prediction to get the animal type from the dictionary
+                    Fraud = (int)prediction[0];
+                }
+                else
+                {
+                    ViewBag.Prediction = "Error: Unable to make a prediction.";
                 }
 
-                predictions.Add(new OrderPrediction {Orders = record.Order, Customer = record.Customer});
             }
 
-            return View(predictions);
+            if (Fraud == 0)
+            {
+                _brickRepository.AddOrder(order);
+                return View("Confirmation");
+            }
+            else
+            {
+                _brickRepository.AddOrder(order);
+                return View("Review");
+            }
+            // predictions.Add(new OrderPrediction {Orders = record.Order, Customer = record.Customer});
+            //}
         }
 
         //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
