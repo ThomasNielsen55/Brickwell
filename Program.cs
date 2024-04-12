0a17e883fab64be496954499746f5fab4de69c0b
Thomas Nielsen <94662330+ThomasNielsen55@users.noreply.github.com>
2024-04-12 03:33:15 -0600

    program

--- Program.cs
+++ Program.cs
@@ -7,186 +7,120 @@ using Brickwell.Components;
 
 public class Program
 {
-    public static async Task Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-        var services = builder.Services;
-        var configuration = builder.Configuration;
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+		var services = builder.Services;
 
-        services.AddAuthentication().AddGoogle(googleOptions =>
-        {
-            googleOptions.ClientId = configuration["Authentication__Google__ClientId"];
-            googleOptions.ClientSecret = configuration["Authentication__Google__ClientSecret"];
-        });
+		var configuration = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json")
+			.Build();
 
-        builder.Services.Configure<CookiePolicyOptions>(options =>
-        {
-            // This lambda determines whether user consent for non-essential 
-            // cookies is needed for a given request.
-            options.CheckConsentNeeded = context => true;
-            options.MinimumSameSitePolicy = SameSiteMode.None;
-            options.ConsentCookieValue = "true";
-        });
+		var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-        // Add services to the container.
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        builder.Services.AddDbContext<BrickDbContext>(options =>
-            options.UseSqlServer(connectionString));
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+		services.AddDbContext<BrickDbContext>(options =>
+			options.UseSqlServer(connectionString));
 
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-            .AddRoles<IdentityRole>()
-            .AddEntityFrameworkStores<BrickDbContext>();
-        builder.Services.AddControllersWithViews();
+		services.AddDefaultIdentity<IdentityUser>(options =>
+			options.SignIn.RequireConfirmedAccount = true)
+			.AddEntityFrameworkStores<BrickDbContext>();
 
-        builder.Services.Configure<IdentityOptions>(options =>
-        {
-            // Default Password settings.
-            options.Password.RequireDigit = true;
-            options.Password.RequireLowercase = true;
-            options.Password.RequireNonAlphanumeric = true;
-            options.Password.RequireUppercase = true;
-            options.Password.RequiredLength = 12;
-            options.Password.RequiredUniqueChars = 1;
-        });
+		services.AddControllersWithViews();
 
+		services.AddScoped<IBrickRepository, EFBrickRepository>();
+		services.AddTransient<ColorViewComponent>();
 
+		services.Configure<IdentityOptions>(options =>
+		{
+			options.Password.RequireDigit = true;
+			options.Password.RequireLowercase = true;
+			options.Password.RequireNonAlphanumeric = true;
+			options.Password.RequireUppercase = true;
+			options.Password.RequiredLength = 12;
+			options.Password.RequiredUniqueChars = 1;
+		});
 
-        builder.Services.AddScoped<IBrickRepository, EFBrickRepository>();
-        builder.Services.AddTransient<ColorViewComponent>();
+		services.AddHsts(options =>
+		{
+			options.Preload = true;
+			options.IncludeSubDomains = true;
+		});
 
-        builder.Services.AddRazorPages();
-
-        builder.Services.AddHsts(options =>
-        {
-            options.Preload = true;
-            options.IncludeSubDomains = true;
-        });
-
-        builder.Services.AddDistributedMemoryCache();
-        builder.Services.AddSession();
+		services.AddDistributedMemoryCache();
+		services.AddSession();
 
 
-        var app = builder.Build();
+		var app = builder.Build();
 
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseMigrationsEndPoint();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Home/Error");
-            app.UseHsts();
-        }
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseDeveloperExceptionPage();
+			app.UseMigrationsEndPoint();
+		}
+		else
+		{
+			app.UseExceptionHandler("/Home/Error");
+			app.UseHsts();
+		}
 
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-        app.UseCookiePolicy();
+		app.UseHttpsRedirection();
+		app.UseStaticFiles();
+		app.UseCookiePolicy();
 
-        // Add Content Security Policy middleware directly here
-        app.Use(async (context, next) =>
-        {
-            context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-            context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
-            context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+		app.UseRouting();
+		app.UseAuthentication();
+		app.UseAuthorization();
 
-            string csp = "default-src 'self'; " +
-             "script-src 'self' 'unsafe-inline'; " +
-             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
-             "img-src 'self' 'unsafe-inline' data: " +
-             "https://m.media-amazon.com " +
-             "https://www.lego.com " +
-             "https://images.brickset.com " +
-             "https://www.brickeconomy.com; " +  // Added image sources here
-             "font-src 'self' https://fonts.gstatic.com;";
+		app.MapControllerRoute(
+			name: "default",
+			pattern: "{controller=Home}/{action=Index}/{id?}");
+		app.MapRazorPages();
 
-
-            if (!context.Response.Headers.ContainsKey("Content-Security-Policy"))
-            {
-                context.Response.Headers.Add("Content-Security-Policy", csp);
-            }
-
-            await next();
-        });
-
-        app.UseSession();
-
-        app.UseRouting();
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.Run();
-    }
-
-    private static void ConfigureServices(IServiceCollection services)
-    {
-        var configuration = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json")
-        .Build();
-
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
-
-        // Add your service registrations here
-        services.AddDbContext<BrickDbContext>(options =>
-            options.UseSqlServer(connectionString));
-
-        services.AddDatabaseDeveloperPageExceptionFilter();
-
-        services.AddDefaultIdentity<IdentityUser>(options =>
-            options.SignIn.RequireConfirmedAccount = true)
-            .AddEntityFrameworkStores<BrickDbContext>();
-
-        services.AddControllersWithViews();
-
-        services.AddScoped<IBrickRepository, EFBrickRepository>();
-        services.AddTransient<ColorViewComponent>();
-        using(var scope = app.Services.CreateScope()) 
-        {
-            var roleManager = 
-                scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-            var roles = new[] { "Admin", "Manager", "Member"}; // Maybe add jsut Admin and Customer
-
-            foreach (var role in roles) 
-            {
-                if (!await roleManager.RoleExistsAsync(role))
-                    await roleManager.CreateAsync(new IdentityRole(role));
-            }
-        }
-
-
-        using (var scope = app.Services.CreateScope())
-        {
-            var userManager =
-                scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-        services.Configure<IdentityOptions>(options =>
-        {
-            options.Password.RequireDigit = true;
-            options.Password.RequireLowercase = true;
-            options.Password.RequireNonAlphanumeric = true;
-            options.Password.RequireUppercase = true;
-            options.Password.RequiredLength = 12;
-            options.Password.RequiredUniqueChars = 1;
-        });
-
-        services.AddHsts(options =>
-        {
-            options.Preload = true;
-            options.IncludeSubDomains = true;
-        });
-
-        services.AddDistributedMemoryCache();
-        services.AddSession();
-    }
+		app.Run();
+	}
 }
+//	private static void ConfigureServices(IServiceCollection services)
+//	{
+//		var configuration = new ConfigurationBuilder()
+//			.SetBasePath(Directory.GetCurrentDirectory())
+//			.AddJsonFile("appsettings.json")
+//			.Build();
 
-        app.MapRazorPages();
+//		var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-        app.Run();
-    }
+//		services.AddDbContext<BrickDbContext>(options =>
+//			options.UseSqlServer(connectionString));
 
-}
+//		services.AddDefaultIdentity<IdentityUser>(options =>
+//			options.SignIn.RequireConfirmedAccount = true)
+//			.AddEntityFrameworkStores<BrickDbContext>();
+
+//		services.AddControllersWithViews();
+
+//		services.AddScoped<IBrickRepository, EFBrickRepository>();
+//		services.AddTransient<ColorViewComponent>();
+
+//		services.Configure<IdentityOptions>(options =>
+//		{
+//			options.Password.RequireDigit = true;
+//			options.Password.RequireLowercase = true;
+//			options.Password.RequireNonAlphanumeric = true;
+//			options.Password.RequireUppercase = true;
+//			options.Password.RequiredLength = 12;
+//			options.Password.RequiredUniqueChars = 1;
+//		});
+
+//		services.AddHsts(options =>
+//		{
+//			options.Preload = true;
+//			options.IncludeSubDomains = true;
+//		});
+
+//		services.AddDistributedMemoryCache();
+//		services.AddSession();
+//	}
+//}
+
+
+
