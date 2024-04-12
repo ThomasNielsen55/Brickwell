c64b1d6605c9bd78cfd2961ffe5e403437372e41
Sawyereva <106629658+Sawyereva@users.noreply.github.com>
2024-04-12 01:22:50 -0600

    yuh

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Brickwell.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    //[Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private readonly ILogger<AdminController> _logger;
